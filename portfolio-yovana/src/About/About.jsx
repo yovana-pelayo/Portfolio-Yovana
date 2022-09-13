b6c1dafb37c0b41fe67922180dcb38c7a0698967
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-09-12 19:38:47 -0700

    created an about me component with a p tag for text  and added an image.

---
+++ portfolio-yovana/src/About/About.jsx
@@ -0,0 +1,14 @@
+
+
+function About() {
+    return( 
+        <>
+        <h1>About Me</h1>
+        <img src="assets/yovana.jpg" />
+        <p>Aspiring FullStack Software Developer willing to contribute a fresh approach to designing and maintaining applications while using new technologies and programming languages. I hope to bring creativity, culture, and a fresh pair of eyes to the team.
+            </p>        
+        
+        </>
+    )
+}
+export default About;
--- portfolio-yovana/src/Home/Home.jsx
+++ portfolio-yovana/src/Home/Home.jsx
@@ -1,9 +1,13 @@
+import About from '../About/About';
 import './Home.css';
 
 function Home() {
     return (
         <div>
         <h1> Welcome to my home page</h1>
+            <div className='about-me'>
+        <About/>
+            </div>
         </div>
     )
 }
