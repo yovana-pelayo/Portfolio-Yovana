a365d908e4138b1939fe78ced3b2ce06dc2fa59c
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-09-19 13:44:08 -0700

    image and summary are properly aligned with decent margin

--- portfolio-yovana/src/About/About.jsx
+++ portfolio-yovana/src/About/About.jsx
@@ -5,12 +5,13 @@ function About() {
         <div className='about-container'>
         <h1>About Me</h1>
         <div className='about-box'>
-        <img src="assets/yovana.jpg" />
+        <img src="assets/yovana.jpg" alt='selfie'/>
+        <div className='summary'> 
         <p>Aspiring FullStack Software Developer willing to contribute a fresh approach to designing and maintaining applications while using new technologies and programming languages. I hope to bring creativity, culture, and a fresh pair of eyes to the team.
-            </p>        
+        </p>        
         </div>
-        
         </div>
-    )
+        </div>
+)
 }
 export default About;
