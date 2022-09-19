333104be3d37f508e6f8e994c0d02d6f089c8db1
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-09-19 12:12:41 -0700

    resized profile image and created a border box for the about me section. This included some padding and set display to flex. Might change depending on visual appeal

--- portfolio-yovana/src/About/About.jsx
+++ portfolio-yovana/src/About/About.jsx
@@ -1,14 +1,16 @@
-
+import './About.css';
 
 function About() {
     return( 
-        <>
+        <div className='about-container'>
         <h1>About Me</h1>
+        <div className='about-box'>
         <img src="assets/yovana.jpg" />
         <p>Aspiring FullStack Software Developer willing to contribute a fresh approach to designing and maintaining applications while using new technologies and programming languages. I hope to bring creativity, culture, and a fresh pair of eyes to the team.
             </p>        
+        </div>
         
-        </>
+        </div>
     )
 }
 export default About;
